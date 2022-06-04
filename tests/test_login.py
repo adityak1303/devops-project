7aef9a884a9c541b1065b6d1be281d53bd72be80
I526519 <kiran.b.a@sap.com>
2022-06-04 09:11:22 +0530

    added additional tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -9,6 +9,60 @@ def find_element_by_xpath(xpath):
     return lambda d: driver.find_element(by=By.XPATH, value=xpath)
 
 
+def test_login_wrong_password_error():
+    driver.get(test_setup.LOGIN_PAGE)
+    title = driver.title
+    assert title != ""
+    WebDriverWait(driver, timeout=10).until(find_element_by_xpath(xpath="/html/body/nav/ul/li[1]/a"))
+    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div[2]/div/div/form/input[1]").send_keys("abcdef@gmail.com")
+    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div[2]/div/div/form/input[2]").send_keys("abcd")
+    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div[2]/div/div/form/input[4]").click()
+    WebDriverWait(driver, timeout=10).until(
+        find_element_by_xpath(xpath="/html/body/div[2]/div/div[2]/div/div/form/div"))
+    danger_string = driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div[2]/div/div/form/div").text
+    print(danger_string)
+    assert danger_string.strip() == "Please check your login details and try again."
+
+def test_login_empty_password_error():
+    driver.get(test_setup.LOGIN_PAGE)
+    title = driver.title
+    assert title != ""
+    WebDriverWait(driver, timeout=10).until(find_element_by_xpath(xpath="/html/body/nav/ul/li[1]/a"))
+    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div[2]/div/div/form/input[1]").send_keys(
+        "abcdef@gmail.com")
+    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div[2]/div/div/form/input[4]").click()
+    password = driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div[2]/div/div/form/input[2]")
+    assert password.text.strip() == ""
+
+
+def test_login_wrong_email_error():
+    driver.get(test_setup.LOGIN_PAGE)
+    title = driver.title
+    assert title != ""
+    WebDriverWait(driver, timeout=10).until(find_element_by_xpath(xpath="/html/body/nav/ul/li[1]/a"))
+    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div[2]/div/div/form/input[1]").send_keys(
+        "abcdef@gmail.co")
+    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div[2]/div/div/form/input[2]").send_keys("kiran")
+    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div[2]/div/div/form/input[4]").click()
+    WebDriverWait(driver, timeout=10).until(
+        find_element_by_xpath(xpath="/html/body/div[2]/div/div[2]/div/div/form/div"))
+    danger_string = driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div[2]/div/div/form/div").text
+    print(danger_string)
+    assert danger_string.strip() == "Please check your login details and try again."
+
+
+def test_login_empty_email_error():
+    driver.get(test_setup.LOGIN_PAGE)
+    title = driver.title
+    assert title != ""
+    WebDriverWait(driver, timeout=10).until(find_element_by_xpath(xpath="/html/body/nav/ul/li[1]/a"))
+    email = driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div[2]/div/div/form/input[1]")
+    email.send_keys("")
+    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div[2]/div/div/form/input[2]").send_keys("kiran")
+    driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/div[2]/div/div/form/input[4]").click()
+    assert email.text.strip() == ""
+
+
 def test_login():
     driver.get(test_setup.LOGIN_PAGE)
     title = driver.title
@@ -44,3 +98,7 @@ def test_locate():
 def test_logout():
     # driver.find_element(by=By.XPATH)
     driver.quit()
+
+
+
+
